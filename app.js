a7e3abeda19cceed8ea42e43b6dc0c169c256def
Tin <bachmach24k@gmail.com>
2022-06-28 15:05:35 -0500

    some more js

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const password = document.querySelector("#password");
 const confirmPass = document.querySelector("#confirm-pass");
 const submit = document.querySelector("button");
 const logo = document.querySelector(".logo");
-
-passReq.remove();
+const mediaQuery = window.matchMedia("(min-width: 961px)");
 
 confirmPass.addEventListener("input", (e) => {
   if (confirmPass.value !== password.value) {
     confirmPass.setCustomValidity("Passwords do not match");
   }
 });
+
+mediaQuery.addEventListener("change", mediaHandler);
+
+function mediaHandler(e) {
+  if (mediaQuery.matches) {
+    passReq.remove();
+    logo.remove();
+    console.log("works!");
+  }
+}
+
+console.log(mediaQuery.matches);
