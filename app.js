65c77044eec358110895b335bbd41ae67505e202
Tin <bachmach24k@gmail.com>
2022-06-28 14:42:32 -0500

    added password check

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const form = document.querySelector("form");
 const passReq = document.querySelector("ul");
+const password = document.querySelector("#password");
+const confirmPass = document.querySelector("#confirm-pass");
+const submit = document.querySelector("button");
+const logo = document.querySelector(".logo");
 
 passReq.remove();
+
+confirmPass.addEventListener("input", (e) => {
+  if (confirmPass.value !== password.value) {
+    confirmPass.setCustomValidity("Passwords do not match");
+  }
+});
